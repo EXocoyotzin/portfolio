1f647dc6269fdb5a9f898ed3df4ca7e0e14334d8
EXocoyotzin <2102395@utrivieramaya.edu.mx>
2024-08-24 12:54:38 -0500

    Begin of the portfolio project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="home">
+    <NavBar/>
+    <v-container fluid>
+      <div class="head">
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative;" class="mt-16">
+              <h1 class="text-white">Holi</h1>
+              <h1 class="text-white">Soy Edgar Xocoyotzin</h1>
+              <span class="text-white">Ing.Gestión administración y arquitectura de software</span><br>
+              <v-btn tile dark class="text-white" variant="outlined">Contactame</v-btn>
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div style="position: absolute;z-index: 9999;bottom:0;
+            margin-left:auto;margin-right:auto;left:0;right:0;
+            text-align:center">
+            <v-icon>fas fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+          <v-col cols="5">
+            <div style="position: relative; z-index: 9999;" class="mt-16">
+              <v-img src="../assets/perrito.jpg" contain max-height="300"></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+import NavBar from '../components/NavBar.vue';
+
+export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    NavBar
+  },
+});
 </script>
+
+<style scoped>
+.v-container {
+  padding-top: 16px 0;
+}
+.head {
+  position: relative;
+  text-align: center;
+  padding:12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+.head:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: rgb(0, 0, 0);
+  transform: skew(0deg, 6deg);
+}
+.head:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+  background: rgb(0, 0, 0);
+  transform: skew(0deg, -6deg);
+}  
+
+</style>
